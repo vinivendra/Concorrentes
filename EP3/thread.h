fe80f69b9478df0f63eb150a607069935cfb621f
Helena <hvictoretti@gmail.com>
2015-06-16 16:29:58 -0300

    Implementação de um monitor, tentado pegar os dois garfos de uma vez.

---
+++ EP3/thread.h
@@ -0,0 +1,15 @@
+
+
+
+void set_n(int novo);
+void set_peso(int peso);
+void set_porcoes(int p);
+void set_deve_ser_uniforme(bool u);
+
+void cria_threads();
+void junta_threads();
+void inicializa_ambiente();
+void imprime_status_final();
+
+
+
